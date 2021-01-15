75e960b1890ff6eee2253d8daec634009d0f5c5e
Cesar Nieto <cesar.nieto@agilethought.com>
2021-01-15 09:35:29 -0600

    Se agrega el nombre de las repos destino

--- src/main/scala/Examen.scala
+++ src/main/scala/Examen.scala
@@ -8,9 +8,9 @@ object Examen {
       .getOrCreate()
 
     val exer = new exercises.Exercises(spark)
-    exer.comicsExecCmd("src/main/resources/output/parquet/")
-    exer.playersExecCmd("src/main/resources/input/csv/fifa/players_20.csv", "src/main/resources/output/parquet/")
-    exer.pokemonExecCmd("src/main/resources/input/csv/pokemon/PokemonData.csv", "src/main/resources/output/parquet/")
+    exer.comicsExecCmd("src/main/resources/output/parquet/comics")
+    exer.playersExecCmd("src/main/resources/input/csv/fifa/players_20.csv", "src/main/resources/output/parquet/players")
+    exer.pokemonExecCmd("src/main/resources/input/csv/pokemon/PokemonData.csv", "src/main/resources/output/parquet/pokemon")
 
   }
 }
