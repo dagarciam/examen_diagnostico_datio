f256b40e982dc28eb17eff44311c3187ba6beabe
Daniel Calixto <daniel.ca.mendoza@gmail.com>
2021-04-18 12:21:35 -0500

    doc(FileStrategy): Add the basic documentation

--- src/main/scala/com/minsait/ttaa/exercises/FileStrategy.scala
+++ src/main/scala/com/minsait/ttaa/exercises/FileStrategy.scala
@@ -1,7 +1,9 @@
 package com.minsait.ttaa.exercises
 
 import org.apache.spark.sql.DataFrame
-
+/** A FileStrategy is an Object that have the logic to read a file
+ *
+ */
 object FileStrategy {
 
   def apply(filePath: String): (String) => Option[DataFrame] =
@@ -10,6 +12,13 @@ object FileStrategy {
       case file => throw new RuntimeException(s"Unknown format: $file")
     }
 
+  /**
+   * csvToDataFrame read a csv file with headers and return a Option[DataFrame] using SparkSession
+   *
+   * @param file is the file path where the file is storage
+   *
+   * @return Option[DataFrame] is the result of read a CSV with SparkSession
+   */
   def csvToDataFrame(file: String): Option[DataFrame] = {
     try{
       Some(SparkCommon
