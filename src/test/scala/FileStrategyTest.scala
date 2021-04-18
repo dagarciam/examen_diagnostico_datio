54fd67081186c07df77aa1985a778c84207e0717
Daniel Calixto <daniel.ca.mendoza@gmail.com>
2021-04-18 12:03:35 -0500

    test(FileStrategy): Add FileStrategy Test

--- src/test/scala/FileStrategyTest.scala
+++ src/test/scala/FileStrategyTest.scala
@@ -1,3 +1,17 @@
-class FileStrategyTest {
+import com.minsait.ttaa.exercises.{FileReader, FileStrategy}
+import org.scalatest.funsuite.AnyFunSuite
+
+class FileStrategyTest extends AnyFunSuite {
+
+  test("The file has an Unknown format [csv]"){
+    assertThrows[RuntimeException]{
+      val readCsv = new FileReader(FileStrategy("test.dat"))
+    }
+  }
+  test("The file doesn't exist, getting a None"){
+    val file = "test.csv"
+    val readCsv = new FileReader(FileStrategy(file))
+    assert(readCsv.read(file) === None)
+  }
 
 }
