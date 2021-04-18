845af33a752bdf0ea4d2d501c108000aa41d974f
Daniel Calixto <daniel.ca.mendoza@gmail.com>
2021-04-18 12:01:47 -0500

    feat(FileStrategy): Add FileStrategy class

--- src/main/scala/com/minsait/ttaa/exercises/FileStrategy.scala
+++ src/main/scala/com/minsait/ttaa/exercises/FileStrategy.scala
@@ -1,5 +1,26 @@
 package com.minsait.ttaa.exercises
 
-class FileStrategy {
+import org.apache.spark.sql.DataFrame
+
+object FileStrategy {
+
+  def apply(filePath: String): (String) => Option[DataFrame] =
+    filePath match {
+      case file if file.endsWith(".csv") => csvToDataFrame
+      case file => throw new RuntimeException(s"Unknown format: $file")
+    }
+
+  def csvToDataFrame(file: String): Option[DataFrame] = {
+    try{
+      Some(SparkCommon
+        .getSparkSession()
+        .read
+        .option("header", true)
+        .csv(file))
+    }catch {
+      case e:Exception => None
+    }
+
+  }
 
 }
