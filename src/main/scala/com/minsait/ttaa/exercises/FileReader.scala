3fb141694977664e8184b8746aba0e11920fe700
Daniel Calixto <daniel.ca.mendoza@gmail.com>
2021-04-18 12:20:49 -0500

    doc(FileReader): Add the basic documentation

--- src/main/scala/com/minsait/ttaa/exercises/FileReader.scala
+++ src/main/scala/com/minsait/ttaa/exercises/FileReader.scala
@@ -1,8 +1,18 @@
 package com.minsait.ttaa.exercises
 
 import org.apache.spark.sql.DataFrame
-
+/** A FileReader that call a strategy to read a file base in the file suffix
+ *
+ *  @constructor create a new FileReader with a strategy
+ *  @param strategy the name of strategy to follow when read a file
+ */
 class FileReader(strategy: (String) => Option[DataFrame]) {
+  /**
+   * Call the strategy to read a file with SparkSession
+   * this return a Option[DataFrame]
+   *
+   * @param filePath is the file path where the file is storage
+   */
   def read(filePath: String): Option[DataFrame] = {
     strategy(filePath)
   }
