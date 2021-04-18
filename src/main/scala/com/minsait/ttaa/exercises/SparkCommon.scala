644f98afc2fe7b8becf4b8aeda211575336dfdc9
Daniel Calixto <daniel.ca.mendoza@gmail.com>
2021-04-18 12:02:32 -0500

    feat(SparkCommon): Add SparkCommon object

--- src/main/scala/com/minsait/ttaa/exercises/SparkCommon.scala
+++ src/main/scala/com/minsait/ttaa/exercises/SparkCommon.scala
@@ -1,5 +1,23 @@
 package com.minsait.ttaa.exercises
 
+import org.apache.spark.sql.SparkSession
+
 object SparkCommon {
+
+  private val master = "local[*]"
+  private val appName = "Exercises"
+
+  def getSparkSession(): SparkSession = {
+    SparkSession
+      .builder()
+      .master("local[*]")
+      .appName(appName)
+      .config("spark.sql.autoBroadcastJoinThreshold", "-1")
+      .getOrCreate()
+  }
+
+  def stopSparkSession(): Unit = {
+    getSparkSession().stop()
+  }
 
 }
